7808ec7aad01cd35cac29b3d16fae98c4dd912e9
Martina G <martii.gb05@gmail.com>
2025-12-08 06:09:17 +0100

    navbar casi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("Probando!");
+// MENÚ
+const burger = document.querySelector(".burger i");
+if (burger) {
+    const nav = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+    burger.classList.toggle("fa-bars");
+    burger.classList.toggle("fa-xmark");
+    nav.classList.toggle("menu-open");
+    });
+}
+
+$(".burger").on("click", function(){
+    $("nav").toggleClass("menu-open");
+});
